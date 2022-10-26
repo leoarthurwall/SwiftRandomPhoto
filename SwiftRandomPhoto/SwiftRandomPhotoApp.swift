0241caf1f092f44d10f6fb38057fb83c8478cbcf
leoarthurwall <leowall93@gmail.com>
2022-10-26 10:56:46 +0100

    Initial Commit

---
+++ SwiftRandomPhoto/SwiftRandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRandomPhotoApp.swift
+//  SwiftRandomPhoto
+//
+//  Created by Leo Wall on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
