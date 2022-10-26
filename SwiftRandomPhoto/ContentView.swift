028d290b4c865e3186d528242f44fefc2a7fc8b7
leoarthurwall <leowall93@gmail.com>
2022-10-26 11:08:40 +0100

    put background collar on a z stack and the text inside of that

--- SwiftRandomPhoto/ContentView.swift
+++ SwiftRandomPhoto/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack {
+                    Color.purple
+                        .ignoresSafeArea()
         Text("Hello, Random Photo App!")
             .padding()
+        }
     }
 }
 
