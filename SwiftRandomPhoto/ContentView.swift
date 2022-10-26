36083a8df58cd9a5f669ec7749b909eff83ca60b
leoarthurwall <leowall93@gmail.com>
2022-10-26 12:46:16 +0100

    added spacers to align the image and button

--- SwiftRandomPhoto/ContentView.swift
+++ SwiftRandomPhoto/ContentView.swift
@@ -16,21 +16,27 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack {
+                Spacer()
+
                 Image(systemName: "photo")
                     .resizable()
                     .foregroundColor(.blue)
                     .frame(width: 300, height: 300)
+                
+                Spacer()
                 
                 Button(action: {
                     //do something
                 }, label: {
                     Text("New Image!")
                         .bold()
-                        .frame(width: 250, height: 40)
+                        .frame(width: 250, height: 50)
                         .foregroundColor(.white)
                         .background(.blue)
                         .cornerRadius(8)
                 })
+                Spacer()
+
             }
             .navigationTitle("Random iPhoto")
         }
